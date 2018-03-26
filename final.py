930470f5d4082e296799a1ec09e80ab2887e1834
rithika <rithzcool@gmail.com>
2018-03-26 13:52:04 +0530

    updated log and added carssatge11.xml

--- final.py
+++ final.py
@@ -4,53 +4,47 @@
 			Sharan Preetha Noronha'''
 
 from cv2 import *
-import RPi.GPIO as GPIO
+from RPi.GPIO import *
 from time import *
 
-# flags indicating if car has been detected in video feed from camera 1 and 2
-f1, f2 = False, False
-last_beep = 0		   # initializing variable to indicate timing of last_beep
+f1, f2, last_beep = False, False, 0  ''' flags indicating if car has been detected in video feed from camera 1 and 2 
+                                         and initializing variable to indicate timing of last_beep '''
 
-
-def beep():			   # called when cars detected on both sides
+def beep():			                   # called when cars detected on both sides
     global last_beep
     if int(time()-last_beep) > 5:      # to ensure gap of atleast 5 seconds between consecutive beeps
-        # consider physical location number i.e pin number not GPIO number
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setwarnings(False)        # disables warnings
-        GPIO.setup(11, GPIO.OUT)       # buzzer 1
-        GPIO.setup(13, GPIO.OUT)       # buzzer 2
-        GPIO.setup(16, GPIO.OUT)        # led 1
-        GPIO.setup(12, GPIO.OUT)        # led 2
+        setmode(BOARD)                 # consider physical location number i.e pin number not GPIO number
+        setwarnings(False)             # disables warnings
+        setup(11, OUT)                 # buzzer 1
+        setup(13, OUT)                 # buzzer 2
+        setup(16, OUT)                 # led 1
+        setup(12, OUT)                 # led 2
         print("Switching LEDs on")
-        GPIO.output(16, GPIO.HIGH)
-        GPIO.output(12, GPIO.HIGH)
-        GPIO.output(11, GPIO.HIGH)
-        GPIO.output(13, GPIO.HIGH)
-        print("Switching buzzers on for 3 seconds")
-        sleep(3)
-        GPIO.output(11, GPIO.LOW)
-        GPIO.output(13, GPIO.LOW)
-        print("Turning LEDs off")
-        GPIO.output(16, GPIO.LOW)
-        GPIO.output(12, GPIO.LOW)
-        last_beep = time()
-        GPIO.cleanup()
+        print("Switching buzzers on for 5 seconds")
+        output(16, HIGH)
+        output(12, HIGH)
+        output(11, HIGH)
+        output(13, HIGH)
+        sleep(5)
+        output(11, LOW)
+        output(13, LOW)
+        print("Turning LEDs and buzzers off")
+        output(16, LOW)
+        output(12, LOW)
+        last_beep = time()             # updating last beep time
+        cleanup()
 
- # capture frames from a video
-cap1 = VideoCapture(0)
+cap1 = VideoCapture(0)    # capture frames from webcams 0 and 1 
 cap2 = VideoCapture(1)
-#cap = VideoCapture("video.avi")
-# XML file describing some features of cars
-car_cascade = CascadeClassifier('cars.xml')
+car_cascade = CascadeClassifier('cascade.xml')  # XML file describing features of cars
 
 while True:
-    ret1, frame1 = cap1.read()   # Reads next frame from video and returns boolean value
+    ret1, frame1 = cap1.read()                # Reads next frame from video and returns boolean value
     ret2, frame2 = cap2.read()
     gray1 = cvtColor(frame1, COLOR_BGR2GRAY)  # Convert to gray scale
     gray2 = cvtColor(frame2, COLOR_BGR2GRAY)
-    cars1 = car_cascade.detectMultiScale(gray1, 1.3, 5)  # Detects cars of different sizes
-    cars2 = car_cascade.detectMultiScale(gray2, 1.3, 5)
+    cars1 = car_cascade.detectMultiScale(gray1, 1.2, 110)  # Detects cars of different sizes
+    cars2 = car_cascade.detectMultiScale(gray2, 1.1, 130)
     print(cars1, cars2)
     for (x, y, w, h) in cars1:   # To draw a rectangle on each car from the current frame
         rectangle(frame1, (x, y), (x+w, y+h), (255, 255, 0), 2)
@@ -58,20 +52,20 @@ while True:
     for (a, b, c, d) in cars2:
         rectangle(frame2, (a, b), (a+c, b+d), (0, 255, 0), 2)
         f2 = True
-    if f1 and f2:
-        print("calling beep")
+    if f1 and f2:                # If car detected in both frames 
+        print("Calling beep")
         beep()
-        f1, f2 = False, False	# Resetting flags
+        f1, f2 = False, False	 # Resetting flags
 
-    namedWindow('Camera 1', WINDOW_NORMAL)    # Displays frame in a 25x25 window
-    resizeWindow('Camera 1', 25, 25)
-    imshow('Camera 1', frame1)
+    namedWindow('Camera 1 - Quantum', WINDOW_NORMAL)    # Displays frame in a 25x25 window
+    resizeWindow('Camera 1 - Quantum', 25, 25)
+    imshow('Camera 1 - Quantum', frame1)
 
-    namedWindow('Camera 2', WINDOW_NORMAL)
-    resizeWindow('Camera 2', 25, 25)
-    imshow('Camera 2', frame2)
+    namedWindow('Camera 2 - Itek', WINDOW_NORMAL)
+    resizeWindow('Camera 2 - Itek', 25, 25)
+    imshow('Camera 2 - Itek', frame2)
 
     if waitKey(33) == 27:   # Wait for Esc key to stop
         break
-
+    
 destroyAllWindows()   # Closes all windows
